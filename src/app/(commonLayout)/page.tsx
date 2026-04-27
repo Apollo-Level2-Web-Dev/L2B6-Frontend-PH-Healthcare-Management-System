ab1ed707b29edf183a1e7b1089dba21cacd8f38f
shafayathub <shafayat.ms@gmail.com>
2026-04-27 14:05:45 +0600

    created the needeed actions and services

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,10 +1,19 @@
-import { Button } from "@/components/ui/button";
+import PublicNavbar from "@/components/modules/home/PublicNavbar";
+import Hero from "@/components/modules/home/Hero";
+import Steps from "@/components/modules/home/Steps";
+import TopDoctors from "@/components/modules/home/TopDoctors";
+import PublicFooter from "@/components/modules/home/PublicFooter";
 
 export default function Home() {
   return (
-    <div>
-        Common
-      <Button variant="outline">Hello World</Button>
+    <div className="min-h-screen flex flex-col">
+      <PublicNavbar />
+      <main className="flex-grow">
+        <Hero />
+        <Steps />
+        <TopDoctors />
+      </main>
+      <PublicFooter />
     </div>
   );
 }
